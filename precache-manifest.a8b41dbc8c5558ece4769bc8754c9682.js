76aad27941f978dfc68ee3db550d5e1d44dd22cd
Jared Tanner <Deraj21@gmail.com>
2019-03-01 21:15:55 -0700

    Updates

---
+++ precache-manifest.a8b41dbc8c5558ece4769bc8754c9682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8cfdc5a679c8633562e",
+    "url": "/simple-weather-app/weather-app/static/js/runtime~main.a8cfdc5a.js"
+  },
+  {
+    "revision": "263a29648cb40ab47846",
+    "url": "/simple-weather-app/weather-app/static/js/main.263a2964.chunk.js"
+  },
+  {
+    "revision": "deca9e114f260aeabf50",
+    "url": "/simple-weather-app/weather-app/static/js/2.deca9e11.chunk.js"
+  },
+  {
+    "revision": "263a29648cb40ab47846",
+    "url": "/simple-weather-app/weather-app/static/css/main.5b570d3c.chunk.css"
+  },
+  {
+    "revision": "aaea69b97902766567ff97c01271b010",
+    "url": "/simple-weather-app/weather-app/index.html"
+  }
+];
